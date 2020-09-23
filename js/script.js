3dc9d7779aec825f61935709e9ebb2eca5b9991c
Walison Miranda <walisonshido@gmail.com>
2020-09-23 12:17:32 -0300

    Código final

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ window.addEventListener('load', start);
 
 var globalNames = ['primeiro', 'segundo', 'terceiro', 'quarto'];
 var inputName = null;
+var isEditing = false;
+var currentIndex = null;
 
 // Ao iniciar a página
 function start(){
@@ -30,12 +32,34 @@ function activateInput(){
     function insertName(newName){
         globalNames.push(newName);
     }
+
+    function updateName(newName){
+        globalNames[currentIndex] = newName;
+    }
+    
     // Captura o dado do formulário
     function handleTyping(event){
-        if (event.key === 'Enter'){
-            insertName(event.target.value);
-            console.log(globalNames);
+
+
+        var hasText = !!event.target.value && event.target.value.trim() !== '';
+
+        if (!hasText){
+            clearInput();
+            return;
         }
+
+        if (event.key === 'Enter'){
+            if (isEditing) {
+                updateName(event.target.value);
+            }
+            else {
+                insertName(event.target.value);
+            }
+            
+            render();
+            isEditing = false;
+            clearInput();
+        } 
     }
        
     inputName.addEventListener('keyup', handleTyping);
@@ -45,24 +69,54 @@ function activateInput(){
 
 // Escrveve os dados na tela
 function render(){
+    function createDeleteButton(index){
+        function deleteName(){
+            globalNames.splice(index, 1);
+            render();
+        }
+
+        var button = document.createElement('button');
+        button.classList.add('deleteButton');
+        button.textContent = 'x';
+        button.addEventListener('click', deleteName);
+
+        return button;
+    }
+
+    function createSpan(name, index){
+        // Editando os dados salvos
+        function editItem(){
+            inputName.value = name;
+            inputName.focus();
+            isEditing = true;
+            currentIndex = index;
+        }
+
+        var span = document.createElement('span');
+        span.classList.add('clickable');
+        span.textContent = name;
+        span.addEventListener('click', editItem);
+
+        return span;
+    }
+
     var divNames = document.querySelector('#names');
     divNames.innerHTML = '';
 
-    // Iteração
     var ul = document.createElement('ul');
     
+    // Iteração
     for (var i = 0; i < globalNames.length; i++){
         var currentName = globalNames[i];
 
         var li = document.createElement('li');
-        var button = document.createElement('button');
-        button.textContent = 'x';
+        var button = createDeleteButton(i);
+        var span = createSpan(currentName, i);
 
-        var span = document.createElement('span');
-        span.textContent = currentName;
 
         li.appendChild(button);
         li.appendChild(span);
+
         ul.appendChild(li);
     }
 
